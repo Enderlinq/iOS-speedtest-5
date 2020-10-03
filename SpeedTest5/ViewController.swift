efe71b9b4270ba8bc545d1c8ef55aba06eed2b61
Enderlinq <Enderlinq@users.noreply.github.com>
2020-10-03 17:13:50 +0300

    Done

--- SpeedTest5/ViewController.swift
+++ SpeedTest5/ViewController.swift
@@ -2,6 +2,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    
+    @IBAction func RedButton(_ sender: UIButton) {
+        red()
+    }
+    
+    @IBAction func blueButton(_ sender: UIButton) {
+        blue()
+    }
+    
+    @IBAction func yellowButton(_ sender: UIButton) {
+        yellow()
+        
+    }
+    
+    @IBAction func greenButton(_ sender: UIButton) {
+        green()
+    }
+    
     @IBAction func red() {
         view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         
@@ -20,6 +42,10 @@ class ViewController: UIViewController {
     @IBAction func green() {
         view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
     }
+    
+
+    
+    
     
     
 }
